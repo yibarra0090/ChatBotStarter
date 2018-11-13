8edc6ab9622d55560cf33a134d83c38d9ccb74b0
BT_1N3_17 <jambroziak9340@bths.edu>
2018-11-13 14:38:02 -0500

    added name

--- src/ChatBot2.java
+++ src/ChatBot2.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 /**
  * A program to carry on conversations with a human user.
  * This version:
- * @author Brooklyn Tech CS Department
- * @version September 2018
+ * @author Juliusz Ambroziak
+ * @version November 2018
  */
 public class ChatBot2
 {
